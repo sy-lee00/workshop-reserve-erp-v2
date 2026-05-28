c8652a4e5dce6e739629917e90d66cd8e5b42904
sy-lee00 <sy_lee00@naver.com>
2026-05-28 18:47:45 +0900

    .

--- e-commerce/frontend/src/customer/components/WorkshopAll/WorkshopAllSlideLogo.js
+++ e-commerce/frontend/src/customer/components/WorkshopAll/WorkshopAllSlideLogo.js
@@ -4,7 +4,7 @@ function WorkshopAllSlideLogo({ workshop }) {
     <div className="workshop-slide-logo">
       {workshop.profileImg != null ? (
         <img
-          src={`${process.env.REACT_APP_API_URL || 'http://localhost:9090'}`/upload/workshop/${workshop.workshopId}/${workshop.profileImg}`}
+          src={`${process.env.REACT_APP_API_URL || 'http://localhost:9090'}/upload/workshop/${workshop.workshopId}/${workshop.profileImg}`}
           alt="워크샵 이미지"
         />
       ) : (
